69ec263b903b5c45992b3c37d79137bfa6f9a99f
amitdesh <amitdesh2014@gmail.com>
2020-07-20 10:13:50 -0400

    Completed methods and console

--- author.rb
+++ author.rb
@@ -1,1 +1,37 @@
+class Author
 
+    attr_accessor :name, :books
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        Book.all.select{|book| book if book.author == self}
+    end
+
+    def write_book(title, word_count)
+        Book.new(title,self,word_count)
+    end
+
+    def total_words
+        books.sum{|book| book.word_count if book.author == self}
+    end
+
+    def self.most_words
+        word_count = {}
+        Author.all.each do |author|
+            words = author.total_words
+            word_count[author] = words
+        end
+        word_count.max_by{|key, value| value}
+    end
+
+end
--- console.rb
+++ console.rb
@@ -1,7 +1,24 @@
-require 'pry'
 require_relative './book'
 require_relative './author'
+require 'pry'
+
+
+
+marx = Author.new("Marx")
+dahl = Author.new("Dahl")
+rowling = Author.new("Rowling")
+
+book1 = Book.new("Das Kapital",marx,1000)
+book2 = Book.new("Communism",marx,50)
+book3 = Book.new("Green Eggs and Ham",dahl,30)
+book4 = Book.new("Harry Potter",rowling,1000)
+
+
+
+
+
 
 binding.pry
+"something"
 
 
